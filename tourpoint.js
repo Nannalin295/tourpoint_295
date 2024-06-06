954c4fa1034c81346806c3dc1b259741e2774e50
nannalin295 <158019811+Nannalin295@users.noreply.github.com>
2024-06-06 23:14:17 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint = 
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "khao yai nationnal park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.50412473478372,
+            14.333888677747652
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand Palace",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49156239677751,
+            13.751562922126496
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Icon siam",
+          "type": "spacial",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.51043650000389,
+            13.726822672510593
+          ],
+          "type": "Point"
+        }
+      },  {
+        "type": "Feature",
+        "properties": {
+          "name": "Paragon Department Store",
+          "type": "spacial",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.53482805153499,
+            13.746582272620543
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Sala Loi",
+          "type": "cutural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            102.11673277116233,
+            14.980659409675624
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Yai Thieng Windmill",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            101.55458927116115,
+            14.784834289569801
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "The Luang Phor Khun Pari-Suttho Sanctuary",
+          "type": "cutural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            101.7368885423283,
+            15.299026734221684
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Thao Suranari (Ya Mo) Monument",
+          "type": "spacial",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            102.11715100837063,
+            14.980920630853007
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Jim Thompson Farm",
+          "type": "natural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            101.86711127116223,
+            14.642672779336607
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Saveone Night Market.",
+          "type": "spacial",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            102.04534099999762,
+            14.957207449113113
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
